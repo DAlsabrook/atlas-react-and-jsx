24db9c0acf695467981e1037f62deb14219cf43f
David Alsabrook <Dfalsabrook@gmail.com>
2025-01-11 19:16:27 +0000

    forgot to change one

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,10 +5,10 @@ export function SocialLinks () {
     return (
         <div className="socialLinks">
             <a href="https://www.linkedin.com/in/david-alsabrook/" target="_blank">
-                <img src="src/assets/linkedin.svg" alt="linkedin logo"></img>
+                <img src={linkedInImage} alt="linkedin logo"></img>
             </a>
             <a href="https://github.com/DAlsabrook" target="_blank">
-                <img src="src/assets/github.svg" alt="github logo"></img>
+                <img src={githubImage} alt="github logo"></img>
             </a>
         </div>
     )
