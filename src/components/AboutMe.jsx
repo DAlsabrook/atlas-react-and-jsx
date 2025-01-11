575db308816b0ec9d247884e22e9c0766f4616a9
David Alsabrook <Dfalsabrook@gmail.com>
2025-01-11 19:09:30 +0000

    pathing in img tag?

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,9 @@
 export function AboutMe () {
     return (
         <div className="about-me">
-            <img src="src/assets/1-backyard_headshot.jpeg" alt="Image of me"></img>
+            <img src="../assets/1-backyard_headshot.jpeg" alt="Image of me"></img>
             <p>I'm a full-stack developer currently in T5 at Atlas School, where I enjoy building web applications and exploring cutting-edge technologies. My favorite framework is Next.js because of its seamless full-stack capabilities. While I'm open to contributing at any level of a development team, my ultimate goal is to transition into a data engineering role after graduation.
                 Outside of coding, I enjoy spending time on my 12-acre property, working on woodworking projects, and relaxing around campfires whenever the weather allows.</p>
         </div>
-    )   
+    )
 }
